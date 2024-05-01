6c7916f0361425837691547b34c846636ce4629f
hemavathitamilmaran <hemavathi.tamilmaran95@gmail.com>
2024-05-01 20:57:16 +0100

    skeleton connect project

---
+++ src/test/java/com/will/amberx/connect/ConnectHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.amberx.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
